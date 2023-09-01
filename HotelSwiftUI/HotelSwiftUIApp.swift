56186be01238bdb6be7d057c777dd8bd24c4f0c9
oomten <oomten@gmail.com>
2023-09-01 19:35:10 +0500

    Initial Commit

---
+++ HotelSwiftUI/HotelSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  HotelSwiftUIApp.swift
+//  HotelSwiftUI
+//
+//  Created by Denis Nurislamov on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
