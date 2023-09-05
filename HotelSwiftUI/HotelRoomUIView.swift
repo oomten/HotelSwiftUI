b09ad5d02fffed540a9aae52152a2b887b875ece
oomten <oomten@gmail.com>
2023-09-06 00:31:49 +0500

    Button for second screen

---
+++ HotelSwiftUI/HotelRoomUIView.swift
@@ -0,0 +1,22 @@
+//
+//  HotelRoomUIView.swift
+//  HotelSwiftUI
+//
+//  Created by Denis Nurislamov on 05.09.2023.
+//
+
+import SwiftUI
+
+struct HotelRoomUIView: View {
+    var body: some View {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationBarTitle("Выбор комнаты")
+        
+    }
+}
+
+struct HotelRoomUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HotelRoomUIView()
+    }
+}
